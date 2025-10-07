0993ee6769dbb58c408c166d85546f7f7024b058
Simon Toivola <simontoivola@gmail.com>
2025-10-07 12:59:38 +0200

    assignment 4 done

---
+++ src/main/java/com/simon/Assignment4/Book.java
@@ -0,0 +1,54 @@
+package com.simon.Assignment4;
+
+import static java.lang.IO.*;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private String numberOfPages;
+
+    public Book() {
+
+    }
+
+    public Book(String title, String author, String numberOfPages) {
+
+        this.title         = title;
+        this.author        = author;
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(String numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", numberOfPages='" + numberOfPages + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/simon/Assignment4/Main.java
@@ -0,0 +1,15 @@
+package com.simon.Assignment4;
+
+// 4. Skapa en klass Book med titel, författare och antal sidor. Skapa en konstruktor med parametrar
+
+import static java.lang.IO.*;
+
+public class Main {
+
+    void main() {
+
+        Book book = new Book("Bible", "Humans", "1286");
+
+        println(book);
+    }
+}
